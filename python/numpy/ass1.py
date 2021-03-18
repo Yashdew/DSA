de66180583656592142b3cfe5f24d01fed679311
Yashdew <yashdewangan123456@gmail.com>
2021-03-18 17:40:35 +0530

    VS crash

--- python/numpy/ass1.py
+++ python/numpy/ass1.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-import re
+#import re
 
-df = pd.read_csv("datasets/dataset_Facebook.csv")
-x = re.sub(";", ",", df)
+df = pd.read_csv("datasets/dataset_Facebook.csv",delimiter=";")
+#x = re.sub(";", ",", df)
 print(df.columns.tolist())
 print(df.head())
 col = df[0:3]
