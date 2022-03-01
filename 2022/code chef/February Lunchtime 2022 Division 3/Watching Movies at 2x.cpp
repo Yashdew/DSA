5bf6616478f7682b1ff4b74517389fc94b45f16f
Yashdew <yashdewangan123456@gmail.com>
2022-03-01 22:13:34 +0530

    Codechef contest #1

---
+++ 2022/code chef/February Lunchtime 2022 Division 3/Watching Movies at 2x.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int X,Y;
+    cin>>X>>Y;
+
+    cout<<X - (Y/2)<<endl;
+}
+int main()
+{  
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+            solve();
+    
+    return 0;
+}
